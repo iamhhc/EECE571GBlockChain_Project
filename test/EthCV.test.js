8895d197d46fd0daa7b1efd758f0a8a5ef6335e5
Zhxq917 <38656558+Zhxq917@users.noreply.github.com>
2021-03-24 13:29:29 -0700

    Update EthCV.test.js

--- test/EthCV.test.js
+++ test/EthCV.test.js
@@ -4,7 +4,7 @@ require('chai')
     .use(require('chai-as-promised'))
     .should();
 
-contract(EthCV, ([deployer, owner, verifier]) => {
+contract(EthCV, ([deployer, user, verifier]) => {
     let ethcv;
     before(async () => {
         ethcv = await EthCV.deployed()
@@ -24,25 +24,60 @@ contract(EthCV, ([deployer, owner, verifier]) => {
         })
     })
 
+    describe('Register', async () => {
+        let result, totalUserNumber
+        before(async () => {
+            result = await ethcv.Register(user, 'tester', 'test@ubc.ca', 'pwd111', 'I am a tester', true)
+            totalUserNumber = await ethcv.totalUserNumber()
+        })
+        it("User registration should be successful if all correct", async () => {
+            truffleAssert.eventEmitted(result, 'LoginSuccess', (ev) => {
+                return ev.user.userAddress.toString() === user.toString()
+                    && ev.user.password === 'pwd111';
+            });
+        })
+        it("List all current users", async () => {
+            const allUsers = await ethcv.getAllUsers()
+            assert.equal(totalUserNumber.toString(), allUsers.length.toString())
+            console.log(allUsers)
+        })
+    })
+
+    describe('Login', async () => {
+        let result
+        before(async () => {
+            result = await ethcv.Login(user, 'pwd111')
+        })
+        it("User login should be successful if all correct", async () => {
+            truffleAssert.eventEmitted(result, 'LoginSuccess', (ev) => {
+                return ev.user.userAddress.toString() === user.toString()
+                    && ev.user.password === 'pwd111';
+            });
+        })
+    })
+
     describe('Create a new record of education', async () => {
         let result, totalNumber, preBalance, preSMBalance
         before(async () => {
             // verifier should be selected from exsiting accounts
-            preBalance = await web3.eth.getBalance(owner)
+            preBalance = await web3.eth.getBalance(user)
             preSMBalance = await web3.eth.getBalance(ethcv.address)
             result = await ethcv.createRecord(
                 '66550476', 'UBC', '', '', '2020-01-01', '2021-01-01', 'Master', 'ECE', verifier, true,
-                {from: owner, value: web3.utils.toWei('1', 'Ether')});
+                { from: user, value: web3.utils.toWei('1', 'Ether') });
             totalNumber = await ethcv.totalNumber()
         })
         it("Creating education record should be successful if all correct", async () => {
             truffleAssert.eventEmitted(result, 'RecordCreated', (ev) => {
                 return ev.recordId.toString() === totalNumber.toString()
-                    && ev.recordOwner.toString() === owner.toString();
+                    && ev.recordOwner.toString() === user.toString()
+                    && ev.record.verifier.toString() === verifier.toString()
+                    && ev.record.isEducation
+                    && ev.record.education.orgName === 'UBC';
             });
         })
-        it("Owner should have the correct balance", async () => {
-            const curBalance = await web3.eth.getBalance(owner)
+        it("User should have the correct balance", async () => {
+            const curBalance = await web3.eth.getBalance(user)
             const difference = new web3.utils.BN(preBalance) - new web3.utils.BN(curBalance).add(new web3.utils.BN(result.receipt.gasUsed))
             assert.equal(difference.toString(), web3.utils.toWei('1'))
         })
@@ -58,34 +93,35 @@ contract(EthCV, ([deployer, owner, verifier]) => {
             // verifier should be selected from exsiting accounts
             result = await ethcv.createRecord(
                 'ali996', 'Alibaba', 'SDE', 'I worked so hard here', '2018-01-01', '2020-01-01', '', '', verifier, false,
-                {from: owner, value: web3.utils.toWei('1', 'Ether')});
+                { from: user, value: web3.utils.toWei('1', 'Ether') });
             totalNumber = await ethcv.totalNumber()
         })
         it("Creating work experience record should be successful if all correct", async () => {
             truffleAssert.eventEmitted(result, 'RecordCreated', (ev) => {
                 return ev.recordId.toString() === totalNumber.toString()
-                    && ev.recordOwner.toString() === owner.toString();
+                    && ev.recordOwner.toString() === user.toString();
             });
         })
     })
 
-    describe('verify a record of work experience', async () => {
+    describe('verify a record of education', async () => {
         let result, totalNumber, preVerifierBalance, preSMBalance
         before(async () => {
             // first create a new record and then verify it.
             await ethcv.createRecord(
-                'testID', 'UBC', '', '', '2020-01-01', '2021-01-01', 'Master', 'ECE', verifier, true,
-                {from: owner, value: web3.utils.toWei('1', 'Ether')});
+                '111111112', 'UBC', '', '', '2020-01-01', '2021-01-01', 'Master', 'ECE', verifier, true,
+                { from: user, value: web3.utils.toWei('1', 'Ether') });
             totalNumber = await ethcv.totalNumber()
             preVerifierBalance = await web3.eth.getBalance(verifier)
             preSMBalance = await web3.eth.getBalance(ethcv.address)
-            result = await ethcv.verifyRecord(totalNumber, {from: verifier});
+            result = await ethcv.verifyRecord(totalNumber, true, { from: verifier });
         })
         it("Verifying work experience record should be successful if all correct", async () => {
             truffleAssert.eventEmitted(result, 'RecordVerified', (ev) => {
                 return ev.recordId.toString() === totalNumber.toString()
-                    && ev.recordOwner.toString() === owner.toString()
-                    && ev.verifier.toString() === verifier.toString();
+                    && ev.recordOwner.toString() === user.toString()
+                    && ev.record.status.toString() === '1'
+                    && ev.record.verifier.toString() === verifier.toString();
             });
         })
         it("Verifier should have the correct balance", async () => {
@@ -98,12 +134,12 @@ contract(EthCV, ([deployer, owner, verifier]) => {
             assert.equal((preSMBalance - curSMBalance).toString(), web3.utils.toWei('0.8'))
         })
         it("A verified record can not be verified again", async () => {
-            await truffleAssert.fails(ethcv.verifyRecord(totalNumber, {
+            await truffleAssert.fails(ethcv.verifyRecord(totalNumber, true, {
                 from: verifier,
             }), truffleAssert.ErrorType.REVERT);
         })
         it("can not verify a record that does not exist", async () => {
-            await truffleAssert.fails(ethcv.verifyRecord(totalNumber + 1, {
+            await truffleAssert.fails(ethcv.verifyRecord(totalNumber + 1, true, {
                 from: verifier,
             }), truffleAssert.ErrorType.REVERT);
         })
@@ -113,17 +149,24 @@ contract(EthCV, ([deployer, owner, verifier]) => {
         let result, totalNumber
         before(async () => {
             //change the status of owner's records
-            result = await ethcv.changeStatus(owner, false, {from: owner});
+            totalNumber = await ethcv.totalNumber();
+            result = await ethcv.changeStatus(totalNumber, { from: user });
         })
         it("Changing the records should be successful if all correct", async () => {
             truffleAssert.eventEmitted(result, 'StatusChanged', (ev) => {
-                return ev.recordOwner.toString() === owner.toString();
+                return ev.recordOwner.toString() === user.toString();
             });
         })
         it("only the owner can change the records", async () => {
-            await truffleAssert.fails(ethcv.changeStatus(owner, false, {from: verifier}), truffleAssert.ErrorType.REVERT);
+            await truffleAssert.fails(ethcv.changeStatus(user, { from: verifier }), truffleAssert.ErrorType.REVERT);
         });
     })
+
+    // TODO: 
+    // 1. Disapprove a record
+    // 2. Change Job Status
+    // 3. Change Description
+    // ....
 
 
 });
