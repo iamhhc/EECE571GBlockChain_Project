cc91e3cc3de06ab7af0a6725874a347c9a772e29
Zhxq917 <38656558+Zhxq917@users.noreply.github.com>
2021-03-23 22:11:42 -0700

    Update EthCV.sol

--- contracts/EthCV.sol
+++ contracts/EthCV.sol
@@ -4,30 +4,37 @@ pragma abicoder v2;
 
 contract EthCV {
     string public appName;
-	uint public totalUserNumber = 0;
+    uint public totalUserNumber = 0;
     uint public totalNumber = 0;
     uint public recordAddPrice = 1 ether;
     uint public recordVerifyAward = 0.8 ether;
 
-	mapping(uint => User) public users;
+    mapping(address => User) private users;
     mapping(uint => Record) public records;
+    // users for searching
+    mapping(address => UserForSearch) public usersWithoutPwd;
 
-	struct User {
-		uint userId;
-		address payable userAddress;
-		string fullName;
-		string email;
-		string password;
-		string selfDescripton;
-		
-		// whenever a record is verified, find the verfier and owner and change below numbers (changes in verify required!)
-		uint   verifiedByNum;    // count the time the user's experiences verified by others
-		uint   hasVerifiedNum;   // count the number of user verifying others
-		
-		bool   isLookingForJobs; // change the status to true if actively looking for jobs
-		Record record;
-	}
-	
+    struct User {
+        address payable userAddress;
+        uint userId;
+        string fullName;
+        string email;
+        string password;
+        string selfDescription;
+
+        // whenever a record is verified, find the verifier and owner and change below numbers (changes in verify required!)
+        uint verifiedByNum;    // count the time the user's experiences verified by others
+        uint hasVerifiedNum;   // count the number of user verifying others
+
+        bool isLookingForJobs; // change the status to true if actively looking for jobs
+    }
+
+    struct UserForSearch {
+        address payable userAddress;
+        uint userId;
+        string fullName;
+    }
+
     struct Record {
         uint recordId;
         address payable recordOwner;
@@ -55,7 +62,7 @@ contract EthCV {
     }
 
     struct Education {
-        // studentID 
+        // studentID
         string uId;
         // school
         string orgName;
@@ -76,84 +83,75 @@ contract EthCV {
         address payable indexed recordOwner,
         address payable verifier
     );
-	
-	event StatusChanged (
-		address payable indexed recordOwner
-	);
+
+    event StatusChanged (
+        address payable indexed recordOwner
+    );
 
     constructor() {
         appName = "EECE571 ETHCV.COM";
     }
-	
-	// Register an account (link a metamask account to an password)
-	function Register(address _ethAccount, string memory _fullName, string _memory _email, string memory _password) public{
-		require(bytes(_ethAccount).length > 0, "account can not be empty");
-		require(bytes(_fullName).length > 0, "name can not be empty");
-		require(bytes(_email).length > 0, "email can not be empty");
-		require(bytes(_password).length > 0, "password can not be empty");
-		User memory _user;
-		totalUserNumber++;
-		_user.userId = totalUserNumber;
-		_user.userAddress = _ethAccount;
-		_user.fullName = _fullName;
-		_user.email = _email;
-		_user.password = _password;
-		_user.selfDescripton = "";
-		_user.verifiedByNum = 0;
-		_user.hasVerifiedNum = 0;
-		_user.isLookingForJobs = false;
-		//_user.record = null?
-		users[totalUserNumber] = _user;
-	}
-	
-	/**
-	* input address and password
-	* if they match, return true, otherwise return false.
-	* front end can get this bool and decide what page the user is going to enter
-	*/
-	function Login(address _ethAccount, string memory _password) public 
-		return isLogin (bool)
-		{
-			require(bytes(_ethAccount).length > 0, "address can not be empty");
-			require(bytes(_password).length > 0, "password can not be empty");
-			for(uint i = 0; i < totalUserNumber; i++){
-				Record memory _user = users[i];
-				if(_user.userAddress == _ethAccount){
-					if(_user.password == _password){
-						return true;
-					}
-					else return false;
-				}
-			}
-	}
-	
-	//change the self description
-	function changeDescription(address _accountAddress, string memory _description) public{
-		require(msg.sender == _accountAddress, "only the account owner can change the description");
-		for(uint i = 0; i < totalUserNumber; i++){
-			Record memory _user = users[i];
-			if(_user.userAddress == _accountAddress){
-				_user.selfDescripton = _description;
-				users[i] = _user;
-		}
-	}
-	
-	//change the job seeking status
-	function changeJobStatus(address _accountAddress, bool _isActive) public{
-		require(msg.sender == _accountAddress, "only the account owner can change the status");
-		for(uint i = 0; i < totalUserNumber; i++){
-			Record memory _user = users[i];
-			if(_user.userAddress == _accountAddress){
-				_user.isLookingForJobs = _isActive;
-				users[i] = _user;
-		}
-	}
-	
+
+    // Register an account (link a metamask account to an password)
+    function Register(address payable _ethAccount, string memory _fullName, string memory _email, string memory _password, string memory _selfDescription, bool _isLookingForJobs) public{
+        require(_ethAccount != address(0), "account can not be empty");
+        require(users[_ethAccount].userId == 0, "account can not be existed");
+        require(bytes(_fullName).length > 0, "name can not be empty");
+        require(bytes(_email).length > 0, "email can not be empty");
+        require(bytes(_password).length > 0, "password can not be empty");
+        User memory _user;
+        totalUserNumber++;
+        _user.userId = totalUserNumber;
+        _user.userAddress = _ethAccount;
+        _user.fullName = _fullName;
+        _user.email = _email;
+        _user.password = _password;
+        _user.selfDescription = _selfDescription;
+        _user.verifiedByNum = 0;
+        _user.hasVerifiedNum = 0;
+        _user.isLookingForJobs = _isLookingForJobs;
+        users[_ethAccount] = _user;
+
+        UserForSearch memory _userForSearch;
+        _userForSearch.fullName = _fullName;
+        _userForSearch.userAddress = _ethAccount;
+        _userForSearch.userId = totalUserNumber;
+        usersWithoutPwd[_ethAccount] = _userForSearch;
+    }
+
+    /**
+    * input address and password
+    * if they match, return true, otherwise return false.
+    * front end can get this bool and decide what page the user is going to enter
+    */
+    function Login(address payable _ethAccount, string memory _password) public returns (bool) {
+        require(_ethAccount != address(0), "account can not be empty");
+        require(bytes(_password).length > 0, "password can not be empty");
+        return keccak256(abi.encodePacked(users[_ethAccount].password)) == keccak256(abi.encodePacked(_password)) ;
+    
+    }
+
+    //change the self description
+    function changeDescription(address payable _accountAddress, string memory _description) public{
+        require(msg.sender == _accountAddress, "only the account owner can change the description");
+        User memory _user = users[_accountAddress];
+        _user.selfDescription = _description;
+        users[_accountAddress] = _user;
+    }
+
+    //change the job seeking status
+    function changeJobStatus(address payable _accountAddress) public{
+        require(msg.sender == _accountAddress, "only the account owner can change the status");
+        User memory _user = users[_accountAddress];
+        _user.isLookingForJobs = !_user.isLookingForJobs;
+        users[_accountAddress] = _user;
+    }
+
 
     // create a record
     function createRecord(string memory _uId, string memory _orgName, string memory _position,
-        string memory _description, string memory _startMonthYear, string memory _endMonthYear,
-        string memory _degreeName, string memory _fieldsOfStudy, address payable _verifier, bool _isEducation) public payable{
+            string memory _description, string memory _startMonthYear, string memory _endMonthYear,
+            string memory _degreeName, string memory _fieldsOfStudy, address payable _verifier, bool _isEducation) public payable{
 
         require(_verifier != address(0) && msg.sender != _verifier, "Valid verifier address is required");
         require(bytes(_startMonthYear).length > 0, "Start Month Year is required");
@@ -161,18 +159,17 @@ contract EthCV {
         // check payment
         require(msg.value >= recordAddPrice, "Payment should be good");
         Record memory _record;
-        if(!_isEducation) {
+        if (!_isEducation) {
             require(bytes(_uId).length > 0, "Worker Id is required");
             require(bytes(_orgName).length > 0, "Org Name is required");
-            require(bytes(_position).length > 0, "Position is required");   
+            require(bytes(_position).length > 0, "Position is required");
             _record.experience = Experience(_uId, _orgName, _position, _description);
-
         } else {
-            require(bytes(_uId).length > 0, "Student Id is required");   
+            require(bytes(_uId).length > 0, "Student Id is required");
             require(bytes(_orgName).length > 0, "School Name is required");
             require(bytes(_degreeName).length > 0, "Degree Name is required");
-            require(bytes(_fieldsOfStudy).length > 0, "Field of study is required");  
-            _record.education = Education(_uId, _orgName, _degreeName, _fieldsOfStudy, _description);                      
+            require(bytes(_fieldsOfStudy).length > 0, "Field of study is required");
+            _record.education = Education(_uId, _orgName, _degreeName, _fieldsOfStudy, _description);
         }
 
         totalNumber++;
@@ -185,7 +182,7 @@ contract EthCV {
         records[totalNumber] = _record;
         emit RecordCreated(totalNumber, msg.sender);
     }
-    
+
 
     // verify a record
     function verifyRecord(uint _recordId) public payable {
@@ -199,20 +196,20 @@ contract EthCV {
         _record.isVerified = true;
         records[_recordId] = _record;
         _verifier.transfer(recordVerifyAward);
+        // uint verifiedByNum;
+        users[_record.recordOwner].verifiedByNum++;
+        users[_record.verifier].hasVerifiedNum++;
+        // uint hasVerifiedNum;
         emit RecordVerified(_recordId, _record.recordOwner, msg.sender);
     }
 
-    //change status, true means others can see the records, false means they can not
-    function changeStatus(address _recordOwner, bool _isActive) public {
-        require(_recordOwner != address(0) && msg.sender == _recordOwner, "Only the owner can change the status");
-        for(uint i = 0; i < totalNumber; i++){
-            Record memory _record = records[i];
-            if(_record.recordOwner == _recordOwner){
-                //change the status
-                _record.isActive = _isActive;
-                records[i] = _record;
-            }
-        }
-		emit StatusChanged(msg.sender);
+    //change a specific record status, true means others can see the record, false means they can not
+    function changeStatus(uint _recordId) public {
+        require(_recordId > 0 && _recordId <= totalNumber, "Record should exist");
+        Record memory _record = records[_recordId];
+        require(msg.sender == _record.recordOwner, "Only the owner can change the status");
+        _record.isActive = !_record.isActive;
+        records[_recordId] = _record;
+        emit StatusChanged(msg.sender);
     }
 }
